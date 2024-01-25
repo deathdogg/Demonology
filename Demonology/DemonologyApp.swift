ff5a524cce88c90f4b1d0bfab2c9026b4f4edd32
Ricardo Herrera Reyes <proherrera22@gmail.com>
2024-01-24 20:20:36 -0800

    Initial Commit

---
+++ Demonology/DemonologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemonologyApp.swift
+//  Demonology
+//
+//  Created by Ricardo Herrera on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct DemonologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
